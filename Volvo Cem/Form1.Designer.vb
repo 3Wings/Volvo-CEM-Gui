f59d51791d9676fd55db18d3846a34fd8b340dc0
3Wings <76914785+3Wings@users.noreply.github.com>
2022-06-21 18:14:29 +0200

    Uploaded the files
    Not fully working, just a start

---
+++ Volvo Cem/Form1.Designer.vb
@@ -0,0 +1,407 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.TabPage3 = New System.Windows.Forms.TabPage()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.ListBox3 = New System.Windows.Forms.ListBox()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        Me.TabPage3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(17, 3)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(79, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Com Port:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(17, 221)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(72, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Cem Pin:"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(141, 24)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(118, 35)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Refresh"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(15, 66)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(118, 35)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "CONNECT"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 20
+        Me.ListBox1.Location = New System.Drawing.Point(261, 8)
+        Me.ListBox1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(919, 264)
+        Me.ListBox1.TabIndex = 4
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(15, 282)
+        Me.ProgressBar1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(1165, 35)
+        Me.ProgressBar1.TabIndex = 5
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(1068, 325)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(112, 35)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "EXIT"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(15, 28)
+        Me.ComboBox1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(118, 28)
+        Me.ComboBox1.TabIndex = 7
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(15, 246)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(118, 26)
+        Me.TextBox1.TabIndex = 8
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(17, 332)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(57, 20)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Label3"
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Controls.Add(Me.TabPage3)
+        Me.TabControl1.Location = New System.Drawing.Point(0, 1)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(1201, 401)
+        Me.TabControl1.TabIndex = 10
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.ProgressBar1)
+        Me.TabPage1.Controls.Add(Me.Label1)
+        Me.TabPage1.Controls.Add(Me.Button3)
+        Me.TabPage1.Controls.Add(Me.Label3)
+        Me.TabPage1.Controls.Add(Me.ListBox1)
+        Me.TabPage1.Controls.Add(Me.ComboBox1)
+        Me.TabPage1.Controls.Add(Me.Button1)
+        Me.TabPage1.Controls.Add(Me.Label2)
+        Me.TabPage1.Controls.Add(Me.TextBox1)
+        Me.TabPage1.Controls.Add(Me.Button2)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 29)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(1193, 368)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Volvo CEM PIN"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.ListBox3)
+        Me.TabPage2.Controls.Add(Me.Label5)
+        Me.TabPage2.Controls.Add(Me.Label4)
+        Me.TabPage2.Controls.Add(Me.TextBox2)
+        Me.TabPage2.Controls.Add(Me.Label6)
+        Me.TabPage2.Controls.Add(Me.ComboBox2)
+        Me.TabPage2.Controls.Add(Me.Label7)
+        Me.TabPage2.Controls.Add(Me.TextBox3)
+        Me.TabPage2.Controls.Add(Me.Button5)
+        Me.TabPage2.Controls.Add(Me.Button4)
+        Me.TabPage2.Controls.Add(Me.Button6)
+        Me.TabPage2.Controls.Add(Me.ComboBox3)
+        Me.TabPage2.Controls.Add(Me.Button7)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 29)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(1193, 368)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Firmware Update"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'TabPage3
+        '
+        Me.TabPage3.Controls.Add(Me.ListBox2)
+        Me.TabPage3.Location = New System.Drawing.Point(4, 29)
+        Me.TabPage3.Name = "TabPage3"
+        Me.TabPage3.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage3.Size = New System.Drawing.Size(1193, 368)
+        Me.TabPage3.TabIndex = 2
+        Me.TabPage3.Text = "About"
+        Me.TabPage3.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(19, 150)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(74, 20)
+        Me.Label5.TabIndex = 31
+        Me.Label5.Text = "Com port"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(19, 28)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(61, 20)
+        Me.Label4.TabIndex = 30
+        Me.Label4.Text = "Hex file"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(19, 236)
+        Me.TextBox2.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(120, 26)
+        Me.TextBox2.TabIndex = 29
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(19, 211)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(79, 20)
+        Me.Label6.TabIndex = 28
+        Me.Label6.Text = "Baud rate"
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(19, 113)
+        Me.ComboBox2.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(241, 28)
+        Me.ComboBox2.TabIndex = 27
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(19, 88)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(57, 20)
+        Me.Label7.TabIndex = 26
+        Me.Label7.Text = "Device"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(19, 53)
+        Me.TextBox3.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(196, 26)
+        Me.TextBox3.TabIndex = 25
+        '
+        'Button5
+        '
+        Me.Button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button5.Location = New System.Drawing.Point(149, 171)
+        Me.Button5.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(112, 35)
+        Me.Button5.TabIndex = 23
+        Me.Button5.Text = "Refresh"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button4.Location = New System.Drawing.Point(149, 276)
+        Me.Button4.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(112, 35)
+        Me.Button4.TabIndex = 22
+        Me.Button4.Text = "Backup"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button6.Location = New System.Drawing.Point(19, 276)
+        Me.Button6.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(112, 35)
+        Me.Button6.TabIndex = 21
+        Me.Button6.Text = "Flash"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'ComboBox3
+        '
+        Me.ComboBox3.FormattingEnabled = True
+        Me.ComboBox3.Location = New System.Drawing.Point(19, 175)
+        Me.ComboBox3.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.ComboBox3.Name = "ComboBox3"
+        Me.ComboBox3.Size = New System.Drawing.Size(120, 28)
+        Me.ComboBox3.TabIndex = 20
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(226, 48)
+        Me.Button7.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(36, 35)
+        Me.Button7.TabIndex = 19
+        Me.Button7.Text = "..."
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.ItemHeight = 20
+        Me.ListBox2.Items.AddRange(New Object() {"About info.....", "Software idea by forhike", "", "Alot to do before software is ready...", "", "Made for..", "CEM Cracker:  https://github.com/vtl/volvo-cem-cracker", "", "Forum: https://www.matthewsvolvosite.com/forums/viewtopic.php?f=10&t=85611"})
+        Me.ListBox2.Location = New System.Drawing.Point(8, 16)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(622, 304)
+        Me.ListBox2.TabIndex = 0
+        '
+        'ListBox3
+        '
+        Me.ListBox3.FormattingEnabled = True
+        Me.ListBox3.ItemHeight = 20
+        Me.ListBox3.Items.AddRange(New Object() {"This part is just mangled from a old hex loader project,", "Basicly a gui for avrdude.exe", "uses devices.txt for settings", "", "Idea was to be able to read and write hex firmware to Tensy 4.0", "", "Have not done any research if avrdude will work or wht settings to use..."})
+        Me.ListBox3.Location = New System.Drawing.Point(377, 28)
+        Me.ListBox3.Name = "ListBox3"
+        Me.ListBox3.Size = New System.Drawing.Size(756, 284)
+        Me.ListBox3.TabIndex = 32
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1200, 403)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "Form1"
+        Me.Text = "Volvo CC - CEM P2 & P3 Pin Cracker"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage1.PerformLayout()
+        Me.TabPage2.ResumeLayout(False)
+        Me.TabPage2.PerformLayout()
+        Me.TabPage3.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Button3 As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents TabPage3 As TabPage
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents ComboBox3 As ComboBox
+    Friend WithEvents Button7 As Button
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents ListBox3 As ListBox
+End Class
---
+++ Volvo Cem/Form1.vb
@@ -0,0 +1,279 @@
+﻿Imports System
+Imports System.IO.Ports
+Imports System.IO
+
+Public Class Form1
+    Dim fileName As String
+    Dim arduinoFlash As String
+    Dim arduinoBackup As String
+    Dim getDate As String = Date.Today
+    Dim MyAvrudeConfPathName As String = IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "avrude.conf")
+    Dim MyAvrudeExePathName As String = IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "avrude.exe")
+    Dim MyLibusbPathName As String = IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "libusb0.dll")
+    'Dim MyBackupMsg As String = IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), " !!!!!")
+
+    'array of devices "devices.txt"
+    Dim substringsArray As New List(Of String)()
+
+
+
+
+
+
+
+
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+
+        ' Declare Ports 
+        Dim Ports As String() = IO.Ports.SerialPort.GetPortNames()
+        ' Add port name Into a comboBox control
+        Dim test As Integer = Ports.GetLength(test)
+        If test <> 0 Then
+            For Each Port In Ports
+                ComboBox1.Items.Add(Port)
+            Next Port
+            ' Select an item in the combobox 
+            ComboBox1.SelectedIndex = 0
+        End If
+
+
+
+
+        Label3.Text = "Disconnected, Please Select Correct COM and press Connect"
+        Button2.Text = "Connect"
+
+        ProgressBar1.Minimum = 0
+        ProgressBar1.Maximum = 100
+        ProgressBar1.Value = 0
+
+
+        ComboBox3.Items.Clear()
+        ' Declare Ports 
+        Dim Ports2 As String() = IO.Ports.SerialPort.GetPortNames()
+        ' Add port name Into a comboBox control
+        Dim test2 As Integer = Ports2.GetLength(test2)
+        If test2 <> 0 Then
+            For Each Port In Ports2
+                ComboBox3.Items.Add(Port)
+            Next Port
+            ' Select an item in the combobox 
+            ComboBox3.SelectedIndex = 0
+        End If
+
+        'IO.File.WriteAllBytes(MyAvrudeConfPathName, My.Resources.avrdude_conf)
+        'IO.File.WriteAllBytes(MyAvrudeExePathName, My.Resources.avrdude_exe)
+        'IO.File.WriteAllBytes(MyLibusbPathName, My.Resources.libusb0)
+
+
+        'ComboBox2.Items.AddRange(System.IO.File.ReadAllLines("devices.txt"))
+        Dim reader = File.OpenText("devices.txt")
+        Dim line As String = ""
+
+        While (reader.Peek() <> -1)
+            line = reader.ReadLine()
+            substringsArray.Add(line)
+            Dim substrings() As String = line.Split(";")
+            ComboBox2.Items.Add(substrings(0))
+        End While
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ComboBox1.Items.Clear()
+        ' Declare Ports 
+        Dim Ports As String() = IO.Ports.SerialPort.GetPortNames()
+        ' Add port name Into a comboBox control
+        Dim test As Integer = Ports.GetLength(test)
+        If test <> 0 Then
+            For Each Port In Ports
+                ComboBox1.Items.Add(Port)
+            Next Port
+            ' Select an item in the combobox 
+            ComboBox1.SelectedIndex = 0
+        End If
+
+        Label3.Text = "Updating COM Ports, Please Select Correct COM and press Connect"
+
+    End Sub
+
+
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        If ComboBox1.SelectedText.ToString() = "-1" Then
+            Label3.Text = "Connected to COM, Please wait progress may take aprox 30 minutes."
+            Button2.Text = "Abort"
+            ProgressBar1.Value = 1
+
+            Do
+
+                Threading.Thread.Sleep(25000)
+                ProgressBar1.PerformStep()
+
+            Loop Until ProgressBar1.Value >= ProgressBar1.Maximum
+
+        Else
+            Label3.Text = "No COM Port Selected, Please select correct COM and Press Connect"
+
+        End If
+
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Dim myFileDlog As New OpenFileDialog()
+
+        'look for files in the c drive
+        myFileDlog.InitialDirectory = "c:\"
+
+        'specifies what type of data files to look for
+        myFileDlog.Filter = "firmware (*.hex)|*.hex"
+
+        'specifies which data type is focused on start up
+        myFileDlog.FilterIndex = 2
+
+        'Gets or sets a value indicating whether the dialog box restores the current directory before closing.
+        myFileDlog.RestoreDirectory = True
+
+        'seperates message outputs for files found or not found
+        If myFileDlog.ShowDialog() =
+            DialogResult.OK Then
+            If Dir(myFileDlog.FileName) <> "" Then
+                fileName = myFileDlog.FileName
+                MsgBox("File to flash: " &
+                       myFileDlog.FileName,
+                       MsgBoxStyle.Information)
+            Else
+                MsgBox("File Not Found",
+                       MsgBoxStyle.Critical)
+            End If
+        End If
+        TextBox3.Text = fileName
+    End Sub
+
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
+
+    End Sub
+
+    Private Sub ComboBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox2.SelectedIndexChanged
+        Dim int As Integer = 0
+        int = ComboBox2.SelectedIndex
+        Dim line As String = substringsArray(int)
+        Dim substrings() As String = line.Split(";")
+        TextBox2.Text = substrings(3)
+    End Sub
+
+    Private Sub ComboBox3_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox3.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        ComboBox3.Items.Clear()
+        ' Declare Ports 
+        Dim Ports As String() = IO.Ports.SerialPort.GetPortNames()
+        ' Add port name Into a comboBox control
+        Dim test As Integer = Ports.GetLength(test)
+        If test <> 0 Then
+            For Each Port In Ports
+                ComboBox3.Items.Add(Port)
+            Next Port
+            ' Select an item in the combobox 
+            ComboBox3.SelectedIndex = 0
+        End If
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+
+        Dim int As Integer = 0
+        int = ComboBox2.SelectedIndex
+
+        If int <> -1 Then
+            If fileName <> "" Then
+                Dim line As String = substringsArray(int)
+                Dim substrings() As String = line.Split(";")
+                arduinoFlash = MyAvrudeExePathName + " -C " + MyAvrudeConfPathName + " -v -p " + substrings(1) + " -c " + substrings(2) + " -P " + ComboBox1.Text + " -b " + TextBox2.Text + " -D -Uflash:w:" + fileName + ":i"
+                If fileName <> "" Then
+                    Shell(arduinoFlash, vbNormalFocus)
+                    TextBox3.Text = " Flashing device"
+
+                End If
+            Else
+                If int <> -1 Then
+                    TextBox3.Text = ""
+                    TextBox3.Text = " Select COM Port and flash"
+
+                Else
+                    TextBox3.Text = ""
+                    TextBox3.Text = " Select device and file"
+                End If
+            End If
+        Else
+            If fileName <> "" Then
+                TextBox3.Text = ""
+                TextBox3.Text = " Select COM Port and device"
+            Else
+                TextBox3.Text = ""
+                TextBox3.Text = " Select device and file"
+            End If
+        End If
+
+        'TextBox1.Text = arduinoFlash
+
+
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+        Dim int As Integer = 0
+        int = ComboBox2.SelectedIndex
+
+        If int <> -1 Then
+            If ComboBox1.Text <> "" Then
+
+                Dim line As String = substringsArray(int)
+                Dim substrings() As String = line.Split(";")
+                arduinoBackup = MyAvrudeExePathName + " -C " + MyAvrudeConfPathName + " -v -p " + substrings(1) + " -c " + substrings(2) + " -P " + ComboBox1.Text + " -b " + TextBox2.Text + " -D -U flash:r:backup_" + getDate + ".hex:i"
+
+                If ComboBox1.Text <> "" Then
+                    Shell(arduinoBackup, vbNormalFocus)
+                    TextBox3.Text = " Backup saved to backup_" + getDate + ".hex"
+                End If
+            Else
+                If int <> -1 Then
+                    TextBox3.Text = ""
+                    TextBox3.Text = " Select COM Port"
+
+                Else
+                    TextBox3.Text = ""
+                    TextBox3.Text = " --------"
+                End If
+            End If
+        Else
+            If ComboBox1.Text <> "" Then
+                TextBox3.Text = ""
+                TextBox3.Text = " Select device"
+            Else
+                TextBox3.Text = ""
+                TextBox3.Text = " Select device and COM Port"
+            End If
+        End If
+
+        'TextBox1.Text = arduinoFlash
+
+
+    End Sub
+
+    Private Sub ListBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox2.SelectedIndexChanged
+
+    End Sub
+End Class
